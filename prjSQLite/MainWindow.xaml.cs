43a631ae4ed4a0938a59002bace4949c1be6245d
Bartosz Porada <bporada@outlook.com>
2020-05-05 08:54:58 +0200

    Projektdateien hinzufügen.

---
+++ prjSQLite/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data;
+
+namespace prjSQLite
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            txtNachname.Clear();
+            txtVorname.Clear();
+            txtStrasse.Clear();
+            txtPLZ.Clear();
+            txtOrt.Clear();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (DTGrid.ItemsSource == null)
+                {
+                    DTGrid.ItemsSource = SqlDB.LoadData().DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            SqlDB.AddData(txtVorname.Text, txtNachname.Text, txtStrasse.Text, txtPLZ.Text, txtOrt.Text);
+        }
+
+        private void DTGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            txtID.Text = ((DataRowView)DTGrid.SelectedItem).Row[0].ToString();
+            txtVorname.Text = ((DataRowView)DTGrid.SelectedItem).Row[1].ToString();
+            txtNachname.Text = ((DataRowView)DTGrid.SelectedItem).Row[2].ToString();
+            txtStrasse.Text = ((DataRowView)DTGrid.SelectedItem).Row[3].ToString();
+            txtPLZ.Text = ((DataRowView)DTGrid.SelectedItem).Row[4].ToString();
+            txtOrt.Text = ((DataRowView)DTGrid.SelectedItem).Row[5].ToString();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            SqlDB.Update(txtID.Text, txtVorname.Text, txtNachname.Text, txtStrasse.Text, txtPLZ.Text, txtOrt.Text);
+            DTGrid.ItemsSource = null;
+            DTGrid.ItemsSource = SqlDB.LoadData().DefaultView;
+        }
+    }
+}
---
+++ prjSQLite/SqlDB.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Controls;
+using System.Windows;
+using System.Data.SqlClient;
+
+namespace prjSQLite
+{
+    class SqlDB
+    {
+        public static SQLiteConnection sqlconn = new SQLiteConnection("Data Source=.\\dbVerwaltung.db");
+        public static SQLiteCommand sqlcmd;
+        public static DataTable sqlDt = new DataTable();
+        public static DataSet DS = new DataSet();
+        public static SQLiteDataAdapter DA;
+
+        
+        public static DataTable LoadData()
+        {
+            sqlDt.Clear();
+            sqlconn.Open();
+            sqlcmd = sqlconn.CreateCommand();
+            sqlcmd.CommandText = "SELECT * FROM Kunden";
+            using (DA = new SQLiteDataAdapter(sqlcmd.CommandText, sqlconn))
+            {
+                DA.Fill(sqlDt);
+                sqlconn.Close();
+                return sqlDt;
+            }        
+        }
+
+        public static void AddData(params string[] arrParam)
+        {
+            try
+            {
+                sqlconn.Open();
+                sqlcmd = sqlconn.CreateCommand();
+                sqlcmd.CommandText = "INSERT INTO Kunden (Vorname, Nachname, Strasse, PLZ, Ort)" +
+                                     "VALUES (@newVorname, @newNachname, @newStrasse, @newPLZ, @newOrt)";
+                sqlcmd.Parameters.AddWithValue("@newVorname", arrParam[0]);
+                sqlcmd.Parameters.AddWithValue("@newNachname", arrParam[1]);
+                sqlcmd.Parameters.AddWithValue("@newStrasse", arrParam[2]);
+                sqlcmd.Parameters.AddWithValue("@newPLZ", arrParam[3]);
+                sqlcmd.Parameters.AddWithValue("@newOrt", arrParam[4]);
+                sqlcmd.ExecuteNonQuery();
+                sqlconn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+}
+
+        public static void Update(params string[] arrParam)
+        {
+            //try
+            //{
+                sqlconn.Open();
+                sqlcmd = sqlconn.CreateCommand();
+                sqlcmd.CommandText = "UPDATE Kunden set Vorname=@newVorname, Nachname=@newNachname, " +
+                                     "Strasse=@newStrasse, PLZ=@newPLZ, Ort=@newOrt " +
+                                     "WHERE KundeID=@ID";
+                sqlcmd.Parameters.AddWithValue("@ID", arrParam[0]);
+                sqlcmd.Parameters.AddWithValue("@newVorname", arrParam[1]);
+                sqlcmd.Parameters.AddWithValue("@newNachname", arrParam[2]);
+                sqlcmd.Parameters.AddWithValue("@newStrasse", arrParam[3]);
+                sqlcmd.Parameters.AddWithValue("@newPLZ", arrParam[4]);
+                sqlcmd.Parameters.AddWithValue("@newOrt", arrParam[5]);
+                sqlcmd.ExecuteNonQuery();
+                sqlconn.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, ex.Source, MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+}
+
+
+
+    }
+}
